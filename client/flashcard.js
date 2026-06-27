7d1dc9ced9acfaa255127ffcde7b2329f5850662
Rithik <kingdeepakrithik@gmail.com>
2026-06-27 18:27:26 +0530

    Create flashcard.js

---
+++ client/flashcard.js
@@ -0,0 +1,30 @@
+const flashcards=[
+
+{
+question:"HTML stands for?",
+answer:"Hyper Text Markup Language"
+},
+
+{
+question:"CSS is used for?",
+answer:"Styling Web Pages"
+},
+
+{
+question:"JavaScript runs on?",
+answer:"Browser"
+},
+
+{
+question:"React is a?",
+answer:"JavaScript Library"
+},
+
+{
+question:"Node.js is?",
+answer:"JavaScript Runtime"
+}
+
+];
+
+export default flashcards;
